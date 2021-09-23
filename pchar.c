1edd166e7f015c27a51872c0a6ef8fef74aac7dc
yassinebid <85518784+yassinebid@users.noreply.github.com>
2021-09-23 22:27:56 +0100

    Create pchar.c

--- pchar.c
+++ pchar.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  *pchar - prints the char at the top of the stack
  *@stack: the pointer to the top of the stack
@@ -6,17 +7,20 @@
  *
  *Return: void
  */
+
 void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (stack  == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		
 		exit(EXIT_FAILURE);
 	}
 
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*stack)->n);
